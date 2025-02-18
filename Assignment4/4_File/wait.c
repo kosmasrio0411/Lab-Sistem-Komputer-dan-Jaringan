7d1d1b010722d8ca3f9641e8b3c38292bf3f6213
Kosmas Rio Legowo <kosmasrio.legowo@gmail.com>
2025-02-19 01:55:40 +0700

    Add files via upload

---
+++ Assignment4/4_File/wait.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+int main()
+{
+	pid_t p;
+	printf("Starting the fork\n");
+	p = fork();
+	// block code for the child process
+	if(p == 0) {
+		printf("I am a child process: %d\n", getpid());
+		printf("My parent id is %d\n", getppid());
+	}
+	// block of code for the parent process
+	else {
+		printf("I am parent process: %d\n", getpid());
+		printf("My child id is %d\n", p);
+		
+		wait(NULL);
+		sleep(15);
+	}
+}
